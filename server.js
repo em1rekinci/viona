3072ddcb292a9adc58a9511d6f4e65d6059d62b1
em1rekinci <ekincianaliz@gmail.com>
2026-03-12 01:31:22 +0300

    bitt

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
 const express = require('express');
 const app = express();
+
+app.use((req, res, next) => {
+  if (req.headers.host === 'vionasoft.com') {
+    return res.redirect(301, 'https://www.vionasoft.com' + req.url);
+  }
+  next();
+});
+
 app.use(express.static('.'));
 app.listen(process.env.PORT || 3000);
